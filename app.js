01945b8539089e8bb43da849e8f014aa805cdf25
BerkayAA <atesberkay2356@gmail.com>
2022-07-17 00:48:01 +0300

    node 4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { json } = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 
 const tourRouter = require('./routers/tourRouters');
@@ -10,22 +11,22 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('hello from the middleware');
-    next();
+  console.log('hello from the middleware');
+  next();
 });
 
-//* enviromental variable usage 
+//* enviromental variable usage
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-//* because of in real senario we do not use files for data manipulations we do not write and delete all off this user 
-//* data based operations to the files 
+//* because of in real senario we do not use files for data manipulations we do not write and delete all off this user
+//* data based operations to the files
 
 //? app.get('/api/v1/tours',getAllTours);
 //? app.post('/api/v1/tours', saveTour);
@@ -40,10 +41,8 @@ app.use((req, res, next) => {
 // app.route('/api/v1/users').get(getAllUsers).post(createUser);
 // app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
-//* a lot more professional usage 
+//* a lot more professional usage
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
-
-
--- server.js
+++ server.js
@@ -1,10 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-console.log(process.env);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('connection is succesfull'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.9,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiking2git ',
+  rating: 4.8,
+  price: 22,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((e) => {
+    console.log(`💥 ${e}`);
+  });
 
 const port = process.env.PORT || 3000;
 
